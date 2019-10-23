37a97ca69b5df598b487fb1bbc640a8ba10854ba
mohamedhassanmn <hassanaesthete008@gmail.com>
2019-10-23 12:14:25 +0530

    initiated

---
+++ frontend/say-cheese/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Seeall from './Seeall'
+export default class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Seeall style={{display:"flex"}}/>
+            </div>
+        )
+    }
+}
---
+++ frontend/say-cheese/src/components/Toggle.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class Toggle extends React.Component{
+    render(){
+        return(
+                <h1></h1>                     
+        )
+    }
+}
